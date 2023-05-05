c53fb044e98931147b56be062c87bb441a3dcf6a
Bill0853 <unfunniestman@gmail.com>
2023-05-05 11:38:49 -0400

    added attack

--- Personal Project/Assets/Scripts/EnemyGrMedium.cs
+++ Personal Project/Assets/Scripts/EnemyGrMedium.cs
@@ -8,24 +8,51 @@ public class EnemyGrMedium : MonoBehaviour
     [SerializeField] float health = 90.0f;
     private bool playerNear;
     private float radius = 20;
-     
+
+    private GameObject rb;
     [SerializeField] SphereCollider triggerCollider;
     [SerializeField] GameObject player;
 
+    [SerializeField] GameObject projectile;
+    [SerializeField] float launchDelay = 3.0f;
+    public bool attack;
+    public float timer;
+    
     // Start is called before the first frame update
     void Start()
     {
+        
         playerNear = false;
         player = GameObject.Find("Player");
+        timer = 0;
+        attack = true;
+
     }
 
     void Update()
-    {
+    { 
+        timer += Time.deltaTime;
+        
         if (playerNear)
         {
             transform.LookAt(player.transform.position);
-        }
+            
+            //attack manager
+            // Add the time since Update was last called to the timer.
+           
 
+            // This will trigger an action every 2 seconds
+            if (attack && timer >= launchDelay)
+            {
+                timer -= launchDelay;
+                attack = false;
+                LaunchProjectile();
+            }
+            else if (!attack && timer >= launchDelay)
+            {
+                attack = true;
+            }
+        }
     }
 
     void OnTriggerEnter(Collider collision)
@@ -34,6 +61,8 @@ public class EnemyGrMedium : MonoBehaviour
         {
             playerNear = true;
             triggerCollider.radius = radius + 10;
+
+
         }
     }
     void OnTriggerExit(Collider collision)
@@ -44,7 +73,10 @@ public class EnemyGrMedium : MonoBehaviour
             triggerCollider.radius = radius;
         }
     }
+    void LaunchProjectile() 
+    { 
+        Instantiate(projectile, rb.transform.position);
+        
+    }
 
-    
-    
 }
